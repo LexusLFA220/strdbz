c5277a062ec10c6cbbfb40f9b79ca19460918f32
LexusLFA220 <alex.amin@lc.cuny.edu>
2023-12-17 14:54:48 -0500

    implemented form to have useState from react and created name and url handle changes

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,14 +1,30 @@
+import {useState} from 'react'
+
 function Form(){
+    const [name,setName] = useState("")
+    const [URL,setURL] = useState("")
+
+    const handleNameChange = (event) => {
+        console.log(event.target.value)
+        setName(event.target.value)
+    }
+
+    const handleURLChange = (event) => {
+        console.log(event.target.value)
+        setURL(event.target.value)
+    }
+
     return(
         <form onSubmit={(event)=>{
             event.preventDefault()
+            console.log(name,URL)
         }}>
             <label for="linkName">Fighter Name:</label>
-            <input type="text" id="linkName" name="linkName" />
+            <input type="text" id="linkName" name="linkName" onChange={handleNameChange} />
             <br />
             <br />
             <label for="URL">Information URL:</label>
-            <input type="text" id="linkURL" name="linkURL"  />
+            <input type="text" id="linkURL" name="linkURL" onChange={handleURLChange}  />
             <br />
             <br />
             <input type="submit" value="Submit"></input>
